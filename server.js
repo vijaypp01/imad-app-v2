a287a99161b8d8dae1995bd04eab2000c48ee7a5
vijaypp01 <vijaypp01@gmail.com>
2017-02-14 23:44:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,14 +80,6 @@ app.get('/:articleName', function(req,res) {
     res.send(createTemplate(articles[atricleName]));
 });
 
-app.get('/article-two', function(req,res) {
-     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req,res) {
-     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
