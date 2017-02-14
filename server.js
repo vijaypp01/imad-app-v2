636e26b131bba1fedba8c08c697739ddfe753892
vijaypp01 <vijaypp01@gmail.com>
2017-02-14 21:39:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function(req,res) {
-    res.send('you have requesttes to article one');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/article-two', function(req,res) {
