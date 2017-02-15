644a24d1c5d610313f61d9c0e2960bd2b82c7a94
vijaypp01 <vijaypp01@gmail.com>
2017-02-15 23:39:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,23 @@
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
 button.onclick = function() {
     
     //Make a Request to the counter endpoint
+    var request = new XMLHttpRequest();
     
     //capture the response
-    
-    //render the variable
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange =  function() {
+        if(request.readyState === XMLHttpRequest.Done){
+            //Take an action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    //Make a request
+    request.open('GET','http://cijaypp01.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
