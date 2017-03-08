2fb7edbd4fe67c73eb3ddbf3bd5a037f02122887
vijaypp01 <vijaypp01@gmail.com>
2017-03-08 18:56:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config = {
+    user: 'vijaypp01',
+    database:'vijaypp01',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -67,8 +75,22 @@ function createTemplate (data) {
     `;
     return htmltemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+   //make aselect request
+   //return a result
+   pool.query('SELECT * FROM test', function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else {
+           res.send(JSON.stringify(result));
+       }
+   });
 });
 
 app.get('/ui/style.css', function (req, res) {
