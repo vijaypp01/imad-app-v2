e649664940abcd621b7b2c7d2aaff7cd43e15f8d
vijaypp01 <vijaypp01@gmail.com>
2017-03-08 19:14:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,11 +105,24 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/:articleName', function(req,res) {
+app.get('/articles/:articleName', function(req,res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article One
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    
+    pool.query("SELECT * FROM articles WHERE title = "+ req.params.articleName,function (err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length === 0){
+                res.status(404).send('Article not found');
+            }else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
+    
 });
 
 
