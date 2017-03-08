b5d2309ab451910c551e030cfa5c03bc39b99b1a
vijaypp01 <vijaypp01@gmail.com>
2017-03-08 18:59:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.get('/test-db',function(req,res){
        if(err){
            res.status(500).send(err.toString());
        } else {
-           res.send(JSON.stringify(result));
+           res.send(JSON.stringify(result.rows));
        }
    });
 });
