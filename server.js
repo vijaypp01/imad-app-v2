59a9c6672621d4cc5722bd1ced779636658a831f
vijaypp01 <vijaypp01@gmail.com>
2017-03-11 21:24:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 
 var config = {
     user: 'vijaypp01',
@@ -13,6 +14,7 @@ var config = {
 }
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 var counter = 0;  
 app.get('/counter', function (req, res) {
@@ -73,6 +75,30 @@ app.get('/hash/:input',function(req,res){
     var hashedString = hash(req.params.input,'this-is-some-radom-string');
     res.send(hashedString);
 });
+
+
+app.post('/create-user',function(req,res){
+    //username,password
+    //JSON
+    ver username = req.body.username;
+    var password = req.body.password;
+    var salt = crypto.getRandombytes(128).toString('hex');
+    var dbString = hash(password,salt);
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+        if(err){
+           res.status(500).send(err.toString());
+       } else {
+           res.send('User successfullt created: '+username;
+       }
+    });
+});
+
+
+
+
+
+
+
 
 
 
