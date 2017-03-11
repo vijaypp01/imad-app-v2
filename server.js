91a3c4a644adc2ce12ad5642683897e424c8adfa
vijaypp01 <vijaypp01@gmail.com>
2017-03-11 20:51:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
+var crypto = require('crypto');
 
 var config = {
     user: 'vijaypp01',
@@ -61,6 +62,19 @@ function createTemplate (data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+function hash(input){
+    //craeting hash
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input',function(req,res){
+    var hashedString = hash(req.params.input,'this-is-some-radom-string');
+    res.send(hashedString);
+});
+
+
 
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
