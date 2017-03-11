80e24010f4362aafe4ee20d78da563b526a67812
vijaypp01 <vijaypp01@gmail.com>
2017-03-11 23:03:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,33 @@ app.post('/create-user',function(req,res){
     });
 });
 
+app.post('/login',function(req,res)){
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query('SELECT * FROM "user" username = $1',[username],function(err,result){
+        if(err){
+           res.status(500).send(err.toString());
+       } else {
+           if(result.rows.length === 0 ){
+               res.send(403).send('username/password is invalid');
+           }
+           else{
+               //Match the password
+               var dbString = result.rows[0].password;
+               var salt = dbString.split('$')[2];
+               var hashedPassword = hast(password, salt);
+               if(hashedPassword === dbString){
+                   res.send('crendential correct');
+               }else {
+                   res.send(403).send('username/ password is invalid');
+               }
+           
+           }
+       }
+       
+    });
+});
+
 
 
 
